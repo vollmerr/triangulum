0aeabb84b0f03590e0f8574b164e5cfb6ebb85fc
vollmerr <vollmerr@oregontate.edu>
2018-10-22 22:47:35 -0700

    fix circle config, add basic server test

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,5 +1,27 @@
-describe('server', () => {
-  it('should have tests....', () => {
+const request = require('supertest');
 
+const fastify = require('../index');
+
+beforeAll(async () => {
+  await fastify.ready();
+});
+
+afterAll(() => {
+  fastify.close();
+});
+
+describe('server', () => {
+  describe('GET /', () => {
+    xit('should send the client', () => {
+
+    });
+  });
+
+  describe('GET /api/healthcheck', () => {
+    it('should return a `200`', async () => {
+      await request(fastify.server)
+        .get('/api/healthcheck')
+        .expect(200);
+    });
   });
 });
