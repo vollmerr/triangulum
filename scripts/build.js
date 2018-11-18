fc0a692cd4c5e288d1df6e085e7f559ed3694e9c
vollmerr <vollmerr@oregontate.edu>
2018-11-17 21:31:57 -0800

    remove static graph

--- scripts/build.js
+++ scripts/build.js
@@ -8,8 +8,4 @@ console.log(`${chalk.magenta('Building client')}\n`);
 // npm binary based on OS
 const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
 // build folder
-const buildProcess = cp.spawn(npmCmd, ['run', 'build'], { env: process.env, cwd: clientPath, stdio: 'inherit' });
-// for now move static graph into build folder
-buildProcess.on('exit', () => {
-  cp.spawn('cp', ['-r', resolve(__dirname, '..', 'graph'), resolve(__dirname, '..', 'client', 'build', 'static')])
-});
+return cp.spawn(npmCmd, ['run', 'build'], { env: process.env, cwd: clientPath, stdio: 'inherit' });
